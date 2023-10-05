8f80a0dfc6b58034b38e4565925bc933f0a01d12
IqbalSaliAlparissy12 <iqbalsalialparissy@gmail.com>
2023-10-05 15:44:49 +0700

    update bro

--- routers/kelas.js
+++ routers/kelas.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const KelasController = require("../controllers/kelas.controller");
+const tblkelas = require("../models/tblkelas");
 const kelas = express.Router();
 
 
 const kelasController = new KelasController();
-kelas.get("/", kelasController.index);
+kelas.get("/", kelasController.index, async(req,res)=> {
+    try {
+        const kelas = await tblkelas.findAll();
+        res.status(200).json({
+            status: "success",
+            data: kelas
+        })
+    } catch (error) {
+        
+    }
+});
 kelas.get("/add", kelasController.indexAdd);
 kelas.get("/edit", kelasController.indexEdit);
 
